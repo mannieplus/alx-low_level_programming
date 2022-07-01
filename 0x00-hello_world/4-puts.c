6928c3c61f84e8d9ef70b224deb6e058f784c156
Emmanuel Akpan <info.mannieplus.com>
2022-06-30 17:29:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-/
+#include <stdio.h>`
+/*
  * main - Entry point
- *  * Return: Always 0 (sucess)
- *   */
- int main(void)
+ * Return: Always 0 (success)  
+ */
+int main(void)
+
 {
 	char *var = ("\"Programming is like building a multilingual puzzle");
 
