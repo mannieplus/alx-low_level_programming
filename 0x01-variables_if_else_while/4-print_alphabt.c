357cf11fa19488acd00cf71b44cc1d378b59e79a
Emmanuel Akpan <info.mannieplus.com>
2022-07-02 11:20:35 -0700

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,12 +7,16 @@
 int main(void)
 
 {
-	char letter;
+	char c = 'a';
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	putchar(letter);
-	for (letter = 'A'; letter <= 'Z'; letter++)
-	putchar(letter);
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+												}
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
