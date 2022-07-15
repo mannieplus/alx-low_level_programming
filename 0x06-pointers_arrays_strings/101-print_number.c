a896cedd37a9c6a55541630b6fb4fb175ff6ef2c
Emmanuel Akpan <info.mannieplus.com>
2022-07-15 09:49:19 -0700

    function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,19 +11,27 @@
 
 void print_number(int n)
 {
-unsigned int n1;
+	unsigned int m, d, count;
 
-n1 = n;
+	if (n < 0)
+	{
+	_putchar(45);
+	m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+		d = m;
+		count = 1;
+	while (d > 9)
+	{
+	d /= 10;
+	count *= 10;
+	}
+	for (; count >= 1; count /= 10)
 
-if (n < 0)
-{
-_putchar('-');
-n1 = -n;
-}
-
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+	{
+	_putchar(((m / count) % 10) + 48);
+	}
 }
