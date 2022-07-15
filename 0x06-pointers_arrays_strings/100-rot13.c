1ca616d856978b2b0f77ff90a729bb3f19b45c37
Emmanuel Akpan <info.mannieplus.com>
2022-07-14 18:59:54 -0700

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,29 +2,23 @@
 
 /**
  *  * rot13 - Function name
- *
  *   * @s: takes a pointer input type of char string
- *
  *    * Description: Encodes a string using rot13
- *
  *     * Return: endoded string
- *
  */
 
 char *rot13(char *s)
-
 {
-
 	int i, j;
 
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 		'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-		'U', 'V', 'W', 'X', 'Y', 'Z'', 'a', 'b', 'c', 'd', 'e', 'f',
+		'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
 		'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's'
-			't', 'u', 'v', 'w', 'x', 'y' 'z'}
+		't', 'u', 'v', 'w', 'x', 'y' 'z'}
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
 		'X', 'Y','Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+		'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'
 		'x', 'y', 'z', 'a', 'b', 'c',
 		'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
@@ -40,5 +34,4 @@ char *rot13(char *s)
 		}
 	}
 	return (s);
-
 }
