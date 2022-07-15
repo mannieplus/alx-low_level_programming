5b879cee62ca0a088f6efef1b1d562427ef044cd
Emmanuel Akpan <info.mannieplus.com>
2022-07-14 20:47:25 -0700

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Function name
- *
- * Return: 0 if programs runs successfully
- *
+ *  * main - Function name
+ *   * Return: 0 if programs runs successfully
  */
-
 int main(void)
-
 {
+	int n;
 
-	int n, a[5];
+	int a[5];
 
 	int *p;
 
 	a[2] = 1024;
 	p = &n;
+
 	*(p + 5) = 98;
 	printf("a[2] = %d\n", a[2]);
 	return (0);
-
 }
