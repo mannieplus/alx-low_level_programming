2af8a41d0cca3132c122ca5121df8d871df44d8d
Emmanuel Akpan <info.mannieplus.com>
2022-07-20 15:02:22 -0700

    Returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Return the 1 length of astring
+ *
+ *   @s: The string to be measured
+ *
+ *    Return: The length of the string
+ *
+ */
+
+int _strlen_recursion(char *s)
+
+{
+
+	int len = 0;
+
+	if (*s)
+
+	{
+
+		len++;
+
+		len += _strlen_recursion(s + 1);
+
+	}
+
+	return (len);
+
+}
