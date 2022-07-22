98a1966105cd43b9f03f8f7683b2a4514d396043
Emmanuel Akpan <info.mannieplus.com>
2022-07-22 14:02:30 -0700

    program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *   main - Prints all argument
+ *
+ *    @argc: argument count
+ *
+ *     @argv: array of pointers to CLI argument
+ *
+ *      Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	int i;
+
+	for (i = 0; i < argc; ++i)
+
+		printf("%s\n", argv[i]);
+
+	return (0);
+
+}
