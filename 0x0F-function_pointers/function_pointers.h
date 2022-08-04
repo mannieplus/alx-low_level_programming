adf6717c2c6d563f37cc07ef2eba2effea5a7657
Emmanuel Akpan <info.mannieplus@gmail.com>
2022-08-04 18:35:51 +0000

    project file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,9 @@
-#include <stdio.h>
-
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
-
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdlib.h>
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* #ifndef FUNCTION_POINTERS_ */
+#endif
